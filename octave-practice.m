6a11a5895d802ba99d021ec222bd64c1c6ad4a66
odd-box <andysustic91@gmail.com>
2016-05-11 23:03:19 -0500

    Add files via upload

---
+++ octave-practice.m
@@ -0,0 +1,83 @@
+# Created by Octave 4.0.0, Wed May 11 23:01:45 2016 Central Daylight Time <unknown@Odd_Box>
+# name: C
+# type: matrix
+# rows: 7
+# columns: 5
+ 0 0 0 0 0
+ 0 0 0 0 0
+ 3 6 9 12 15
+ 4 8 12 16 20
+ 0 0 0 0 0
+ 0 0 0 0 0
+ 7 14 21 28 35
+
+
+# name: M
+# type: scalar
+4
+
+
+# name: ans
+# type: matrix
+# rows: 1
+# columns: 4
+ 1 2 3 4
+
+
+# name: c
+# type: scalar
+5
+
+
+# name: col
+# type: scalar
+4
+
+
+# name: i
+# type: scalar
+1
+
+
+# name: j
+# type: scalar
+1
+
+
+# name: m
+# type: scalar
+4
+
+
+# name: r
+# type: scalar
+7
+
+
+# name: row
+# type: scalar
+4
+
+
+# name: sum
+# type: scalar
+5
+
+
+# name: x
+# type: matrix
+# rows: 1
+# columns: 4
+ 1 2 3 4
+
+
+# name: z
+# type: matrix
+# rows: 4
+# columns: 4
+ 0 0 0 0
+ 0 0 0 0
+ 0 0 0 0
+ 0 0 0 0
+
+
